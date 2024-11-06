d700dbdd732de8ce744e0ee7a532de72647dc91d
pgilstru <penelope.gilstrap@gmail.com>
2024-11-05 17:45:18 -0700

    created app component

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { Home } from './login/home';
+// import { Login } from './scores/login';
+// import { Register } from './about/register';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+    return <div>App will display here</div>;
+}
