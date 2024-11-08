c71e65b95e5e13091f5afaf29d1db02b847c9255
pgilstru <penelope.gilstrap@gmail.com>
2024-11-08 14:22:40 -0700

    some bootstrap

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-// import { Home } from './login/home';
-// import { Login } from './scores/login';
-// import { Register } from './about/register';
+import { BrowserRouter, NavLink, Route, Routes, useNavigate } from 'react-router-dom';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Register } from './register/register';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 import './app.css';
 
 export default function App() {
   return (
+    <BrowserRouter>
     <div className='body'>
         <header>
             <img className='logo-img' alt='grocerease' src='/grocerease_logo.png' width='325'/>
@@ -15,21 +18,29 @@ export default function App() {
             <span className="user-info">
                 Signed in as:
                 <span>username</span>
-                <form className="logout-form" method="get" action="login.html">
-                    <button className="logout-button btn btn-sm btn-dark" type="submit">Logout</button>
-                </form>
+                <span className="logout-form"><LogoutButton /></span>
             </span>
             {/* <!-- nav bar --> */}
             <nav>
                 <menu>
-                    <li className="nav-item"><a href="index.html">Home</a></li>
+                    <li className="nav-item"><NavLink to="">Home</NavLink></li>
                     <li className="nav-item"><a href="https://docs.google.com/forms/d/e/1FAIpQLSdxF9-nQRfsREiYeliNuDmM2D5pE-gWiKYrkJL2qSX9mdyv9g/viewform?usp=sf_link">Report an Issue</a></li>
+                    {/* <li className="nav-item"><a href="index.html">Home</a></li>
+                    <li className="nav-item"><a href="https://docs.google.com/forms/d/e/1FAIpQLSdxF9-nQRfsREiYeliNuDmM2D5pE-gWiKYrkJL2qSX9mdyv9g/viewform?usp=sf_link">Report an Issue</a></li> */}
                 </menu>
             </nav>
             </div>
         </header>
 
-        <main>App components here</main>
+        {/* <main>App components here</main> */}
+        {/* to */}
+
+        {/* <Routes>
+            <Route path="/" element={<Home />} exact />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes> */}
 
         <footer>
             <div className="div-main">
@@ -38,5 +49,19 @@ export default function App() {
             </div>
         </footer>
     </div>
+    </BrowserRouter>
   );
 }
+
+export function LogoutButton({ }) {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        // perform logout logic here, update when you start having users login
+        navigate('/login'); //redirect to login page
+    }
+    return (
+      <Button className="logout-button btn btn-sm btn-dark" onClick={handleLogout}>
+        Logout
+      </Button>
+    );
+  }
